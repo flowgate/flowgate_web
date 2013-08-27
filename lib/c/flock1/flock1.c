9c3d5d16fca8480293dd5831600cb71444a554f4
Hyunsoo <hkim@jcvi.org>
2013-08-27 10:46:56 -0400

    moved dots information from parameters.txt to fcs.properties

--- lib/c/flock1/flock1.c
+++ lib/c/flock1/flock1.c
@@ -2236,7 +2236,6 @@ int main (int argc, char **argv)
   f_parameters=fopen("parameters.txt","w");
   fprintf(f_parameters,"Number_of_Bins\t%d\n",num_bin);
   fprintf(f_parameters,"Density\t%d\n",den_t_event);
-  fprintf(f_parameters,"Dots\t%d\n",num_nonempty);
   fprintf(f_parameters,"Min\t%d\n",min);
   fprintf(f_parameters,"Max\t%d\n",max);
   fclose(f_parameters);
@@ -2249,6 +2248,7 @@ int main (int argc, char **argv)
   fprintf(f_parameters,"Populations=%d\n",num_population);
   fprintf(f_parameters,"Events=%d\n",file_Len);
   fprintf(f_parameters,"Markers=%d\n",num_dm);
+  fprintf(f_parameters,"Dots=\t%d\n",num_nonempty);
   fclose(f_properties);
 
 
