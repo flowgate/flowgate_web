016161f84f1d05c8010161d012789c41de43e06d
Hyunsoo <hkim@jcvi.org>
2013-10-15 15:58:56 -0400

    Added genepattern job submission Added genepattern server information to properties file Added job id parameter to image runner test files

---
+++ lib/java/flockUtils/test/org/immport/flock/utils/GenePatternTest.java
@@ -0,0 +1,32 @@
+package org.immport.flock.utils;
+
+import org.junit.Test;
+
+import java.util.UUID;
+
+/**
+ * User: hkim
+ * Date: 10/15/13
+ * Time: 2:49 PM
+ * org.immport.flock.utils
+ */
+public class GenePatternTest {
+    GenePattern gp = new GenePattern();
+
+    @Test
+    public void main() throws Exception {
+        String testDir = "/Users/hkim/Stuffs/test/gofcm/";
+        String gpdev_export = "/export/data/results";
+
+        gp.executePipeline(
+                "hkim",
+                testDir + "input1.zip",
+                "10",
+                "11",
+                "10",
+                null,
+                UUID.randomUUID().toString(),
+                gpdev_export
+        );
+    }
+}
