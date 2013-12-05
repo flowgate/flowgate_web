a3b28296ab1c50657bf9aec888f36dd11dd6a176
Hyunsoo <hkim@jcvi.org>
2013-12-05 15:01:54 -0500

    added a function to wait image load added pages to test dataTables plugin

--- js/shared.js
+++ js/shared.js
@@ -69,3 +69,22 @@ function makeAjaxCall(t,u,d,cb) {
     error: function() {}
   });
 }
+
+$.fn.imagesLoaded = function () {
+  $imgs = this.find('img[src!=""]');
+  // if there's no images, just return an already resolved promise
+  if (!$imgs.length) {return $.Deferred.resolve().promise();}
+  // for each image, add a deferred object to the array which resolves when the image is loaded
+  var dfds = [];  
+  $imgs.each(function(){
+    var dfd = $.Deferred();
+    dfds.push(dfd);
+    var img = new Image();
+    img.onload = function(){dfd.resolve();}
+    img.src = this.src;
+  });
+
+  // return a master promise object which will resolve when all the deferred objects have resolved
+  // IE - when all the images are loaded
+  return $.when.apply($,dfds);
+}
